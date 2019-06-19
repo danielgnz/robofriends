5621e62f46d176d52deb26391e97a3804f94434f
Daniel GNZ <danibtechnology@gmail.com>
2019-06-19 11:16:27 +0100

    Updates

---
+++ precache-manifest.47e13443e6d778f597449162aae5cff5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53d2e4c1e0c9a7fa36da74d0cc6fac76",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "2ab60f797bd1ea7adf1c",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "a2b17fe802281f6253f9",
+    "url": "/robofriends/static/css/main.65b4b91f.chunk.css"
+  },
+  {
+    "revision": "2ab60f797bd1ea7adf1c",
+    "url": "/robofriends/static/js/2.b93257fc.chunk.js"
+  },
+  {
+    "revision": "a2b17fe802281f6253f9",
+    "url": "/robofriends/static/js/main.dcd8cd9a.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
